f25dc8e7319a73b7cfcb72f9ec1a1dffab2d0410
Marcelo <marcelohenriquehoffmeister@gmail.com>
2021-04-03 09:07:11 -0400

    board style

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import Square from './Square'
 
+const style = {
+        border: '4px solid darkblue',
+        borderRadius: '10px',
+        width: '250px',
+        height: '250px',
+        margin: '0 auto',
+        display: 'grid',
+        gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)'
+}
+
 const Board = ({ squares, onClick }) => (
-    <div>
+    <div style={style}>
         <Square value="1" onClick={() => onclick('dummy value')} />
         <Square value="2" onClick={() => onclick('dummy value')} />
         <Square value="3" onClick={() => onclick('dummy value')} />
