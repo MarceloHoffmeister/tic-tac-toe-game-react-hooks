1d29bd97fb8074286f2fd1e1b9769b8380fa120b
Marcelo <marcelohenriquehoffmeister@gmail.com>
2021-04-03 08:46:34 -0400

    square component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import './components/Game';
+import Game from "./components/Game";
 
 function App() {
   return (
     <div className="App">
-      Tic Tac Toe
+      <Game />
     </div>
   );
 }
---
+++ src/components/Square.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Square = ({ value, onClick }) => (
+    <button onClick={onClick}>
+        {value}
+    </button>
+)
+
+export default Square
